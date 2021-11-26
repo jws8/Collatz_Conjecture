497dc9f190134c13bce4ca5dbc79e4d83281e145
jws8 <94502439+jws8@users.noreply.github.com>
2021-11-26 14:01:44 -0800

    Update with first graphical representation

--- Collatz_Conjecture.py
+++ Collatz_Conjecture.py
@@ -4,28 +4,42 @@
 #Method to show Collatz pattern based off of a number
 #Input: number to generate Collatz pattern
 #Output:
-#Iterations to show length vs. num 
 #Range of pattern
+#Iterations 
+import matplotlib.pyplot as plt
+import numpy as np
 
 def collatz_conjecture(num):
     conject_list = [num]
+    conj_list7 = [7, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1]
+    iterations = 1
     #While loop to catch repetitive Collatz pattern 4-2-1
     while num > 1:
         #Number is even
         if num % 2 == 0:
             num = num//2
-        #Number is odd   
+            #Number is odd   
         else:
             num = num*3 + 1
+        iterations+=1
         conject_list.append(num)
-
-    print(conject_list)
-    conject_list.sort()
-    print("Range of Collatz Conjecture: [" + str(conject_list[0]) + ", " + 
-    str(conject_list[len(conject_list) - 1]) + "]")
-    print("Iterations in Collatz Conjecture: " + str(len(conject_list)))
+    #numpy to arrange data 
+    y = np.array(conject_list)
+    x = np.arange(0, iterations)
+    u = np.array(conj_list7)
+    v = np.arange(0,17)
+    #plt to draw
+    #labels
+    plt.title("Collatz Conjecture Model Given Integer")
+    plt.xlabel("Iterations")
+    plt.ylabel("Value")
+    plt.plot(x,y, label = "collatz curve n given",linestyle ="-")
+    plt.plot(v,u, label = "comparison with n = 7", linestyle ="--")
+    plt.legend()
+    plt.show()
 
 #Run
 num = int(input("Enter a number for the Collatz Conjecture: "))
 collatz_conjecture(num)
+
 
